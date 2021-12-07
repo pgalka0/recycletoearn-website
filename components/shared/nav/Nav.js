a65b724fbb2f3e6cc12ed8ad39df05bc8d4375d0
c0ndi <kowalik0konrad@gmail.com>
2021-12-07 01:10:25 +0100

    Init

---
+++ components/shared/nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./nav.module.scss"
+
+export default function Nav() {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/styles.scss'
+import '../styles/reset.scss'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
