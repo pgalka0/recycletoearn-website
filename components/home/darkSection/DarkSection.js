ac360ef1723ebab3ae982113d640ba2c33971cb1
c0ndi <kowalik0konrad@gmail.com>
2021-12-12 15:54:20 +0100

    Changes

--- components/home/darkSection/DarkSection.js
+++ components/home/darkSection/DarkSection.js
@@ -25,7 +25,7 @@ function DarkSection() {
                     ></iframe> */}
                     <iframe src='https://my.spline.design/coin-8b032db318fa17a00a20045092ee961b/'
                         loading="lazy"
-                        frameborder='0'
+                        frameBorder='0'
                         width='100%'
                         height='100%'
                         overflow='scroll'
