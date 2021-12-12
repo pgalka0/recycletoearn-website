41fbf4be46a15030932cbcca4b24dc21c535be62
Piotrek <65945270+Piotrek-hub@users.noreply.github.com>
2021-12-12 20:05:31 +0100

    Update web3functions.js

--- Contracts/web3functions.js
+++ Contracts/web3functions.js
@@ -14,7 +14,7 @@ export const handleMetamaskConnection = async (window, setAccount) => {
 
 };
 
-export const addUser = async (address, contract) => {
+export const addUser = async (contract, address) => {
 	try{
 		await contract.methods.addUser()
 			.send({from: address, gas: 3000000})
@@ -29,33 +29,36 @@ export const addUser = async (address, contract) => {
 
 }
 
-export const readUsers = async (contract, setUsers) => {
+export const readUsers = async (contract) => {
 	const users = []
 	try {
-		contract.events.allEvents({fromBlock: 0}, (error, event) => {
-		
-			if(event.event === "userAdded") {
-				users.push({id: event.returnValues.userId, address: event.returnValues.addr, totalScore: 0, plastic: 0, glass: 0, bio: 0}) 
-			}
-			else if(event.event === "userScored"){
-
-				users[event.returnValues.userId].totalScore += event.returnValues.totalScore;
-
-				switch(event.returnValues.achievementType){
-					case 'plastic': 
-						users[event.returnValues.userId].plastic += event.returnValues.specificAchievementScore;
-						break;
-					case 'glass': 
-						users[event.returnValues.userId].plastic += event.returnValues.specificAchievementScore;
-						break;
-					case 'bio': 
-						users[event.returnValues.userId].plastic += event.returnValues.specificAchievementScore;
-						break;
+		contract.getPastEvents({fromBlock: 0}, (error, events) => {
+			events.forEach((event) => {
+				
+				if(event.event === "userAdded") {
+					
+					users.push({id: parseInt(event.returnValues.userId), address: event.returnValues.addr, totalScore: 0, plastic: 0, glass: 0, bio: 0}) 
 				}
-			}
-			setUsers(users)
+				else if(event.event === "userScored"){
 
+					users[event.returnValues.userId].totalScore += event.returnValues.totalScore;
 
+					switch(event.returnValues.achievementType){
+						case 'plastic': 
+							users[event.returnValues.userId].plastic += event.returnValues.specificAchievementScore;
+							break;
+						case 'glass': 
+							users[event.returnValues.userId].plastic += event.returnValues.specificAchievementScore;
+							break;
+						case 'bio': 
+							users[event.returnValues.userId].plastic += event.returnValues.specificAchievementScore;
+							break;
+					}
+				}
+			})
+			
+			console.log(users);
+		
 			
 		})
 		
